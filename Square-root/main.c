350c69f4563e44f93b8d04bcba912991b2d114d2
BENHIMA-Mohamed-Amine <benhima.mohamed.amine@gmail.com>
2023-10-10 22:52:04 +0100

    change

--- Square-root/main.c
+++ Square-root/main.c
@@ -5,7 +5,7 @@ int main(){
     double x, y;
 
     printf("Write a positive number: ");
-    scanf("%f", &y);
+    scanf("%lf", &y);
     x = SquareRoot(y);
 
     printf("The square root of %.1f is %.10f", y, x);
